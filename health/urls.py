1eaa837b637f50c0eace9abf5e5a5e179c7b3dcb
kiarashk8128 <oxfvard@gmail.com>
2024-08-12 10:57:35 +0330

    Accounts: Add homepage view

--- health/urls.py
+++ health/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import homepage
 
 urlpatterns = [
+    path('', homepage, name='homepage'),
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
 ]
