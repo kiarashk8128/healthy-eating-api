db9b184704a377f906ed69784668c353dfc72595
kiarashk8128 <oxfvard@gmail.com>
2024-08-12 13:10:58 +0330

    Accounts: Change Models based on new requirement of Panel Admin.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,16 +1,13 @@
 from rest_framework import serializers
-from .models import CustomUser, Family, FamilyMember
-
+from .models import CustomUser, FamilyMember
 
 class FamilyMemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = FamilyMember
         fields = ['first_name', 'last_name', 'birthday', 'gender', 'height', 'weight']
 
-
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    family_members = FamilyMemberSerializer(many=True, write_only=True, required=False)
+class CustomUserSerializer(serializers.ModelSerializer):
+    family_members = FamilyMemberSerializer(many=True, required=False)
 
     class Meta:
         model = CustomUser
@@ -19,23 +16,8 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         family_members_data = validated_data.pop('family_members', [])
-        user = CustomUser(
-            username=validated_data['username'],
-            email=validated_data.get('email'),
-            first_name=validated_data.get('first_name'),
-            last_name=validated_data.get('last_name'),
-            birthday=validated_data.get('birthday'),
-            gender=validated_data.get('gender'),
-            height=validated_data.get('height'),
-            weight=validated_data.get('weight'),
-            is_family_head=validated_data.get('is_family_head', False)
-        )
-        user.set_password(validated_data['password'])  # Hashes the password
-        user.save()
-
+        user = CustomUser.objects.create_user(**validated_data)
         if user.is_family_head:
-            family = Family.objects.create(head=user)
             for member_data in family_members_data:
-                FamilyMember.objects.create(family=family, **member_data)
-
+                FamilyMember.objects.create(family_head=user, **member_data)
         return user
