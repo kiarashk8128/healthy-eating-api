be4e2691f0349c3dede79b5e74cec6a48ef8e67e
kiarashk8128 <oxfvard@gmail.com>
2024-08-12 10:24:43 +0330

    Accounts: Add Family model

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,29 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    username = models.CharField(unique=True)
     birthday = models.DateField()
     gender = models.CharField(max_length=10, choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')])
     height = models.FloatField()  # Height in cm
     weight = models.FloatField()  # Weight in kg
     is_family_head = models.BooleanField(default=False)  # Indicates if the user is the head of a family
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'birthday', 'gender', 'height', 'weight', 'is_family_head']
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'birthday', 'gender', 'height', 'weight']
 
     def __str__(self):
         return self.username
+
+
+class Family(models.Model):
+    head = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='family_head')
+    members = models.ManyToManyField(CustomUser, related_name='family_members')
+
+    def __str__(self):
+        return f"{self.head.email}'s Family"
