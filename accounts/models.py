530bcd03f9d4e669c1d211f4e62c9fd81c1c7511
kiarashk8128 <oxfvard@gmail.com>
2024-08-12 10:20:44 +0330

    Accounts: Add CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(unique=True)
+    birthday = models.DateField()
+    gender = models.CharField(max_length=10, choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')])
+    height = models.FloatField()  # Height in cm
+    weight = models.FloatField()  # Weight in kg
+    is_family_head = models.BooleanField(default=False)  # Indicates if the user is the head of a family
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'birthday', 'gender', 'height', 'weight', 'is_family_head']
+
+    def __str__(self):
+        return self.username
