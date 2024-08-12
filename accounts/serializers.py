d44e4c0d08b97d4c236c1a24fe7ed65b563c709b
kiarashk8128 <oxfvard@gmail.com>
2024-08-12 10:01:47 +0330

    Accounts: Create serializer for signup

---
+++ accounts/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'birthday')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            birthday=validated_data['birthday']
+        )
+        return user
