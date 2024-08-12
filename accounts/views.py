047aa9783d7a407b4cb21545f281c01d7e3b71bb
kiarashk8128 <oxfvard@gmail.com>
2024-08-12 10:29:18 +0330

    Accounts: Add view and path for signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import UserSerializer
 
-# Create your views here.
+
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response({
+            'username': user.username,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'birthday': user.birthday,
+            'gender': user.gender,
+            'height': user.height,
+            'weight': user.weight,
+            'is_family_head': user.is_family_head
+        }, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
