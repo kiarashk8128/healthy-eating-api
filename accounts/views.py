52dd13448adf7fe23ca5581e26204c4a26e33a99
kiarashk8128 <oxfvard@gmail.com>
2024-08-12 10:58:00 +0330

    Accounts: Add homepage view function

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.http import HttpResponse
 from .serializers import UserSerializer
 
 
@@ -20,3 +21,7 @@ def signup(request):
             'is_family_head': user.is_family_head
         }, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def homepage(request):
+    return HttpResponse("Welcome to the Homepage!")
