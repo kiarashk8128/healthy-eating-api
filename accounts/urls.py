aa8ab9e03dde7d013b4526f432027081ff17cd7f
kiarashk8128 <oxfvard@gmail.com>
2024-08-15 21:22:33 +0330

    Accounts: Add JWT token to login view.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import signup, login_view, homepage
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
-    path('login/', login_view, name='login'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),  # Use JWT login view
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', homepage, name='homepage'),
 ]
