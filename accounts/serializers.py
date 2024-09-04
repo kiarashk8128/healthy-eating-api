ab81742bb4dd4a411818eff1901f9a77e69e9fe6
kiarashk8128 <oxfvard@gmail.com>
2024-09-04 12:33:50 +0330

    Hotfix: add validations in the backend-side for password, email, username.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
 from .models import CustomUser, FamilyMember
 
 
@@ -17,6 +20,23 @@ class CustomUserSerializer(serializers.ModelSerializer):
         fields = ['username', 'password', 'email', 'first_name', 'last_name', 'birthday', 'gender', 'height', 'weight',
                   'is_family_head', 'family_members']
         extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_username(self, value):
+        if CustomUser.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username already exists.")
+        return value
+
+    def validate_email(self, value):
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists.")
+        return value
+
+    def validate_password(self, value):
+        try:
+            validate_password(value)  # This will run all the validators from settings.py
+        except ValidationError as e:
+            raise serializers.ValidationError(str(e))
+        return value
 
     def create(self, validated_data):
         family_members_data = validated_data.pop('family_members', [])
